762b5ae93ab09b777786aecd7f3050040c7639b9
febeBaltus <102193511+febeBaltus@users.noreply.github.com>
2022-04-20 16:12:21 +0200

    Initial commit
    Update main.py
    
    Update world.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from json.encoder import ESCAPE
+import pygame
+from pygame.locals import *
+from world import World
+import sys
+from pynput import keyboard
+
+pygame.init()
+
+
+FPS = pygame.time.Clock()
+
+keys = pygame.key.get_pressed()
+world = World()
+pygame.display.set_caption("Escape-Game")
+
+
+def quit():
+    pygame.display.quit()
+    pygame.quit()
+    sys.exit()
+
+
+while True:
+    FPS.tick(5)
+    world.act()
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit()
+            
+        elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    quit()
---
+++ world.py
@@ -0,0 +1,31 @@
+from tkinter import Y
+from turtle import screensize
+from xmlrpc.client import boolean
+import pygame
+from pygame.locals import *
+from pygame.sprite import Group
+from pygame.locals import *
+import msvcrt
+from astronaut import Player
+
+# SIZE = WIDTH, HEIGHT = 500, 400
+EnableFullScreen = False
+
+pygame.init()
+info = pygame.display.Info()
+SIZE = WIDTH, HEIGHT = info.current_w, info.current_h
+
+
+class World:
+    def __init__(self):
+        DIM = 800
+        self.DISPLAYSURF = pygame.display.set_mode(SIZE)
+        self.background = pygame.image.load("achtergrond3.jpg")
+        self.background = pygame.transform.scale(self.background, SIZE)  # 1920 x 1080
+
+
+    def act(self):
+        self.DISPLAYSURF.blit(self.background, (0, 0))
+
+    def setFullScreen(self):
+        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
